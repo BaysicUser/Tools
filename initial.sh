ef1d828e5f397445e72f8d8fc67a5c7e89a1ea30
BaysicUser <121414289+BaysicUser@users.noreply.github.com>
2025-10-18 11:43:04 -0400

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -41,6 +41,12 @@ python3 -m pipx ensurepath
 # Libreoffice
 #sudo apt-get install libreoffice
 
+# Install Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+sudo apt-get update
+sudo apt-get install sublime-text
+
 # Recon Tools
 echo "#### Downloading Recon Tools ####"
 
